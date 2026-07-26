0625391adef9447a4a210b03f436041be0a2b6dc
Gaurav Balpande <gauravbalpande18@gmail.com>
2026-07-26 20:56:13 +0530

    implementation of pid tracking

--- src/process/models/ProcessInfo.cpp
+++ src/process/models/ProcessInfo.cpp
@@ -1,16 +1,45 @@
 #include "ProcessInfo.h"
 
+#include <sstream>
+
 namespace process
 {
+
+namespace
+{
+std::string statusToString(ProcessStatus status)
+{
+    switch (status)
+    {
+    case ProcessStatus::RUNNING:
+        return "Running";
+    case ProcessStatus::TERMINATED:
+        return "Terminated";
+    default:
+        return "Unknown";
+    }
+}
+} // namespace
 
 ProcessInfo::ProcessInfo(
     uint32_t processId,
     const std::string& processName,
-    uint32_t parentProcessId)
+    uint32_t parentProcessId,
+    ProcessStatus status)
     : processId(processId),
       processName(processName),
-      parentProcessId(parentProcessId)
+      parentProcessId(parentProcessId),
+      status(status)
 {
+}
+
+std::string ProcessInfo::toString() const
+{
+    std::ostringstream out;
+    out << "PID: " << processId << "\n"
+        << "Process: " << processName << "\n"
+        << "Status: " << statusToString(status);
+    return out.str();
 }
 
 } // namespace process
